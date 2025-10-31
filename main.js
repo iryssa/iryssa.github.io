69c99366d21f4a026b36a726a51cb33ed52fa178
grit501 <code.grit501@passfwd.com>
2025-10-31 10:35:25 +0800

    modified:   index.html 	modified:   main.js 	renamed:    main.css -> style.css

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("left").classList.add("active");
   document.getElementById("right").classList.add("active");
   document.getElementById("center").classList.add("active");
@@ -9,19 +9,13 @@ document.addEventListener("DOMContentLoaded", function() {
 const leftNavigator = document.getElementById("left");
 const moreNavigator = document.getElementById("more");
 
-const menuDepartureButton = document.querySelector("button.departure#menu");
-const menuReturnButton = document.querySelector("button.return#menu");
-const settingsDepartureButton = document.querySelector("button.departure#settings");
-const join_usDepartureButton = document.querySelector("button.departure#join_us");
-
-menuDepartureButton.addEventListener("click", function() {
+document.querySelector("button.departure#menu").addEventListener("click", () => {
   leftNavigator.classList.remove("active");
   moreNavigator.classList.add("active");
 });
 
-menuReturnButton.addEventListener("click", function() {
-  const sections = document.querySelectorAll("div.section");
-  sections.forEach(function(section) {
+document.querySelector("button.return#menu").addEventListener("click", () => {
+  document.querySelectorAll("div.section").forEach(section => {
     section.classList.remove("active");
   });
   moreNavigator.classList.remove("active");
@@ -33,20 +27,17 @@ function hideMoreShowLeft() {
   leftNavigator.classList.add("active");
 }
 
-settingsDepartureButton.addEventListener("click", hideMoreShowLeft);
-join_usDepartureButton.addEventListener("click", hideMoreShowLeft);
-
-const sections = document.querySelectorAll("div.section");
+document.querySelector("button.departure#settings").addEventListener("click", hideMoreShowLeft);
+document.querySelector("button.departure#join_us").addEventListener("click", hideMoreShowLeft);
 
 document.querySelectorAll("button.departure").forEach(btn => {
   btn.addEventListener("click", () => {
-    sections.forEach(sec => sec.classList.toggle("active", sec.id === btn.id));
+    document.querySelectorAll("div.section").forEach(sec => sec.classList.toggle("active", sec.id === btn.id));
   });
 });
 
 document.querySelectorAll("button.return").forEach(btn => {
   btn.addEventListener("click", () => {
-    const sec = document.querySelector(`div.section#${btn.id}`);
-    if (sec) sec.classList.remove("active");
+    document.querySelector(`div.section#${btn.id}`).classList.remove("active");
   });
 });
